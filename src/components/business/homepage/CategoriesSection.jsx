345b0ef89fa09cf873ee44dc79ef300d535c1d45
Uday932 <uday.bhavsar.sio@gmail.com>
2025-04-28 23:24:52 +0200

    MUUD-13 add category grill

---
+++ src/components/business/homepage/CategoriesSection.jsx
@@ -0,0 +1,75 @@
+"use client";
+import apiRoutes from "@/apiUtils/apiRoutes";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Image from "@@/ui/Image";
+import Text from "@@/ui/Text";
+import Link from "@@/ui/Link";
+import { useTranslations } from "next-intl";
+
+const CATEGORIES_URL = process.env.NEXT_PUBLIC_CLOUDINARY_CATEGORIES_URL || "";
+
+export default function CategoriesSection() {
+  const [categories, setCategories] = useState([]);
+  const t = useTranslations();
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const response = await axios.get(apiRoutes.categories.all());
+        setCategories(response.data);
+      } catch (error) {
+        console.error("Erreur lors du fetch des catégories :", error);
+      }
+    };
+
+    getCategories();
+  }, []);
+
+  if (categories.length === 0) return null;
+
+  // Trier les catégories par priorité décroissante
+  const sortedCategories = categories.sort((a, b) => {
+    const priorityA = a.priority ?? 0;
+    const priorityB = b.priority ?? 0;
+
+    return priorityB - priorityA;
+  });
+
+  return (
+    <section className="bg-secondary py-16">
+      <div className="container mx-auto px-4">
+        <Text
+          size="title"
+          className="mb-12 text-center font-black uppercase text-primary"
+        >
+          {t("home.ourCategories")}
+        </Text>
+        <div className="grid gap-8 md:grid-cols-3">
+          {sortedCategories.map((category) => (
+            <Link key={category.id} href={category.link} className="block">
+              <div className="relative rounded-lg bg-white p-6 shadow-lg transition-transform hover:scale-105">
+                {/* Titre de la catégorie */}
+                <Text color="black" className="mb-2 font-bold">
+                  {category.name}
+                </Text>
+                {/* Description de la catégorie */}
+                <Text color="gray" className="text-sm">
+                  {category.description}
+                </Text>
+                {/* Image de la catégorie */}
+                <Image
+                  src={`${CATEGORIES_URL}${decodeURIComponent(category.image)}`}
+                  alt={`Image ${category.name}`}
+                  className="mb-4 h-48 w-full rounded-lg object-cover" // Augmentez la hauteur (h-48)
+                  width={400} // Ajustez selon vos besoins
+                  height={300} // Ajustez selon vos besoins
+                />
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
