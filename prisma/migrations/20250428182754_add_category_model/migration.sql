776a9f23e650e0642b75da01bc846f3f4e8c95a2
Uday932 <uday.bhavsar.sio@gmail.com>
2025-04-28 23:23:58 +0200

    MUUD-13 add category in prisma

---
+++ prisma/migrations/20250428182754_add_category_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "image" TEXT,
+    "link" TEXT,
+    "priority" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
