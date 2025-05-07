25694a54ed42e3065b8eac7cea397bd814910c71
Uday932 <uday.bhavsar.sio@gmail.com>
2025-05-07 17:45:47 +0200

    MUUD-32 remove duplicate routes

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -24,12 +24,6 @@ const routes = {
       create: () => "/backoffice/categories/create",
       edit: (id) => `/backoffice/categories/edit/${id}`,
     },
-    categories:{
-      all: () => "/backoffice/categories",
-      single: (id) => `/backoffice/categories/${id}`,
-      create: () => "/backoffice/categories/create",
-      edit: (id) => `/backoffice/categories/edit/${id}`,
-    },
     subscriptions: () => "/backoffice/subscriptions",
     settings: () => "/backoffice/settings",
   },
